e26f74dcfb598c56dde14034ffa301207713da49
David Jennings <dkj.jennings@gmail.com>
2019-06-23 10:04:00 -0700

    Added openpyxl module usage

--- Purchasing_Exception_Report_Generator.py
+++ Purchasing_Exception_Report_Generator.py
@@ -7,11 +7,13 @@ Version 2.0 11/16/2018 changed input from .csv to
 .xlsx'''
 
 import tkinter as tk
-# from tkinter import ttk
 from tkinter import filedialog  # noqa: F401
 import re
 import os
 import xlrd
+from openpyxl import Workbook
+# from openpyxl import load_workbook
+from openpyxl.styles import Border, Side, Alignment, Font, NamedStyle
 
 
 class Purchasing_Exceptions(tk.Tk):
@@ -87,9 +89,6 @@ class Purchasing_Exceptions(tk.Tk):
     def run_analysis(self, event):
         ''' Search input file for part # and negative balance'''
         ds = set()
-        self.results_file = ('{}/Purchasing Exceptions.csv').format(self.idir)
-        with open(self.results_file, 'w') as out_file:
-            out_file.write('')
         wb = xlrd.open_workbook(self.filename)
         sht = wb.sheet_by_index(0)
         ds = set()
@@ -102,29 +101,62 @@ class Purchasing_Exceptions(tk.Tk):
                 ds.add(pnum)
                 ds_list = list(ds)
                 fin = sorted(ds_list)
+        self.part_list = []
         for item in fin:
+            self.part_list.append(item)
             self.file_listbox.insert(tk.END, item)
-            with open(self.results_file, 'a') as out_file:
-                out_file.write(item + '\n')
-        count_string = (('Processing complete. {} files found.')
-                        .format(len(fin)))
-        saved_as_string = ('Results file: {}').format(self.results_file)
-        self.file_listbox.insert(tk.END, count_string)
-        self.file_listbox.insert(tk.END, saved_as_string)
+        self.count_string = (('Processing complete. {} files found.')
+                             .format(len(fin)))
         self.file_listbox.see(tk.END)
         self.open_results_button.bind('<ButtonRelease-1>',
                                       self.open_results_file)
         self.choose_file_button.unbind('<ButtonRelease-1>')
         self.process_file_button.unbind('<ButtonRelease-1>')
+        self.write_to_spreadsheet()
+
+    def write_to_spreadsheet(self):
+        '''Formats and writes data to spreadsheet.'''
+        wb = Workbook()
+        highlight = NamedStyle(name="highlight")
+        highlight.font = Font(bold=False, size=11)
+        bd = Side(style='thin', color="000000")
+        highlight.border = Border(left=bd, top=bd, right=bd, bottom=bd)
+        wb.add_named_style(highlight)  # Register named style
+        sh1 = wb.active
+        sh1.title = 'Purchasing Exceptions'
+        sh1.append(['Part Number'])
+        sh1['A1'].font = Font(bold=True, size=11)
+        sh1['A1'].border = Border(left=bd, top=bd, right=bd, bottom=bd)
+        sh1['A1'].alignment = Alignment(horizontal='center')
+        rnum = 2
+        sh1.column_dimensions['A'].width = (30)
+        # col_width2 = 0
+        for item in self.part_list:
+            # sh1_tool_list_data = item
+            sh1.cell(row=rnum, column=1).value = item
+            sh1.cell(row=rnum, column=1).style = 'highlight'
+            # if len(str(sh1_description)) > col_width2:
+            #     col_width2 = len(str(sh1_description))
+            rnum += 1
+        # sh1.column_dimensions['A'].width = (col_width2 * 1.125)
+        save_name = (('{}/Purchasing Exception Report.xlsx')
+                     .format(self.idir))
+
+        self.saved_as_string = ('Results file: {}').format(save_name)
+        self.file_listbox.insert(tk.END, self.count_string)
+        self.file_listbox.insert(tk.END, self.saved_as_string)
+        self.file_listbox.see(tk.END)
+        wb.save(save_name)
+        os.startfile(save_name)
 
     def open_results_file(self, event):
         """ Open results file in Excel"""
-        os.startfile(self.results_file)
+        os.startfile('Purchasing Exception Report.xlsx')
 
     def open_file(self):
         ''' Open any file'''
         open_filename = (tk.filedialog.askopenfilename(title='Add File',
-                         filetypes=(('CSV Files', '*.csv'),
+                         filetypes=(('Excel Files', '*.xlsx'),
                                     ('All Files', '*.*'))))
         os.startfile(open_filename)
 
